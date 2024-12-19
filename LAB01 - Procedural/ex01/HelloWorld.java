36310871a01c4016c2f6cf581615be3f092baab2
Vinicius Tavares Martins <12321bcc042@facom.local>
2024-12-19 14:31:59 -0300

    exercicio resolvido

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.println("Vinicius Tavares Martins");
+		System.out.println("12321BCC042");
+	}
+}
